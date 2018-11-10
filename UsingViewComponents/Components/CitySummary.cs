fecdb45cbb73bb41c18dc6a9c592a72b0b9b0f64
Jon Perkins <captainperk81@gmail.com>
2018-11-10 16:08:50 -0500

    Deriving from the View Component Base Class

---
+++ UsingViewComponents/Components/CitySummary.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private readonly ICityRepository _cityRepository;
+
+        public CitySummary(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
+
+        public string Invoke() => $"{_cityRepository.Cities.Count()} cities, {_cityRepository.Cities.Sum(c => c.Population)} people.";
+    }
+}
