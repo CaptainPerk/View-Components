d059b7b5d31dce0b6f9afd1ba4db9dbe3a53cc26
Jon Perkins <captainperk81@gmail.com>
2018-11-10 15:16:25 -0500

    Creating the Controller and Views

---
+++ UsingViewComponents/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public ViewResult Index() => View(_productRepository.Products);
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Product newProduct)
+        {
+            _productRepository.AddProduct(newProduct);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- UsingViewComponents/Models/IProductRepository.cs
+++ UsingViewComponents/Models/IProductRepository.cs
@@ -7,17 +7,4 @@ namespace UsingViewComponents.Models
         IEnumerable<Product> Products { get; }
         void AddProduct(Product newProduct);
     }
-
-    public class MemoryProductRepository : IProductRepository
-    {
-        private readonly List<Product> products = new List<Product>
-        {
-            new Product { Name = "Kayak", Price = 275M },
-            new Product { Name = "Lifejacket", Price = 48.95M },
-            new Product { Name = "Soccer Ball", Price = 19.50M }
-        };
-
-        public IEnumerable<Product> Products => products;
-        public void AddProduct(Product newProduct) => products.Add(newProduct);
-    }
 }
