83a25fe989640e4b1799e96fbc06136cd9206fbe
Jon Perkins <captainperk81@gmail.com>
2018-11-10 19:21:28 -0500

    Creating the ViewComponent/Controller Class

---
+++ UsingViewComponents/Controllers/CityController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Collections.Generic;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Controllers
+{
+    [ViewComponent(Name = "ComboComponent")]
+    public class CityController : Controller
+    {
+        private readonly ICityRepository _cityRepository;
+
+        public CityController(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(City newCity)
+        {
+            _cityRepository.AddCity(newCity);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IViewComponentResult Invoke() => new ViewViewComponentResult
+        {
+            ViewData = new ViewDataDictionary<IEnumerable<City>>(ViewData, _cityRepository.Cities)
+        };
+    }
+}
