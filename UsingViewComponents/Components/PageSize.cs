33a41b19eaecf70f5b540b59cb709620bfd3a016
Jon Perkins <captainperk81@gmail.com>
2018-11-10 19:07:54 -0500

    Creating Asynchronous View Components

---
+++ UsingViewComponents/Components/PageSize.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UsingViewComponents.Components
+{
+    public class PageSize : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var httpClient = new HttpClient();
+            var httpResponseMessage = await httpClient.GetAsync("http://apress.com");
+            return View(httpResponseMessage.Content.Headers.ContentLength);
+        }
+    }
+}
